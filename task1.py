9567f7fd37280626bebc785e7be317190a26a37a
jigyanshu agrawal <145665528+delljigyanshu@users.noreply.github.com>
2024-07-28 18:17:40 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,74 +1,74 @@
 #list:
 my_list = [10,20,30,40,50,60,70,80,90,100]
 
-# #adding an element in the list:
+#adding an element in the list:
 
-# my_list.append(110)
-# print(my_list)
+my_list.append(110)
+(my_list)
 
-# #remove an element in the list:
+#remove an element in the list:
 
-# my_list.remove(50)
-# print(my_list)
+my_list.remove(50)
+(my_list)
 
-# #modifying an element in the list:
+#modifying an element in the list:
 
-# my_list[5] = 130
-# print(my_list)
+my_list[5] = 130
+(my_list)
 
-# #reverse the element in the list:
+#reverse the element in the list:
 
-# my_list.reverse()
-# print(my_list)
+my_list.reverse()
+(my_list)
 
 #dictionary:
-# my_dict = {'name':'jigy','age':20,'city':'uttar pradesh'}
+my_dict = {'name':'jigy','age':20,'city':'uttar pradesh'}
 
-# #adding an element in the dictionary:
+#adding an element in the dictionary:
 
-# my_dict['gender']='Male'
-# my_dict['hobby']='guitar'
-# print(my_dict)
+my_dict['gender']='Male'
+my_dict['hobby']='guitar'
+(my_dict)
 
 # #remove an element in the dictionary:
 
-# del my_dict['gender']
-# print(my_dict)
+del my_dict['gender']
+print(my_dict)
 
-# #modify an element in the dictionary:
+#modify an element in the dictionary:
 
-# my_dict['city']='goa'
-# print(my_dict)
+my_dict['city']='goa'
+(my_dict)
 
 
-# #reverse the element in the dictionary:
+#reverse the element in the dictionary:
 
-# reversed_dict = {v: k for k, v in my_dict.items()}
-# print(reversed_dict)
+reversed_dict = {v: k for k, v in my_dict.items()}
+(reversed_dict)
 
 #set:
 my_set = {10,20,30,40,50,50,60,60,70,80,90,100}
 
 #adding an element in the set:
 
-# my_set.add(120)
-# my_set=sorted(my_set)
-# print(my_set)
+my_set.add(120)
+my_set=sorted(my_set)
+print(my_set)
 
 #remove an element in the set:
 
-# my_set.remove(40)
-# my_set=sorted(my_set)
-# print(my_set)
+my_set.remove(40)
+my_set=sorted(my_set)
+print(my_set)
 
 #modify an element in the set:
 
-# my_set.add(140)
-# my_set.discard(20)
-# my_set=sorted(my_set)
-# print(my_set)
+my_set.add(140)
+my_set.discard(20)
+my_set=sorted(my_set)
+print(my_set)
 
-# #reverse an element in the set:
+#reverse an element in the set:
 
 my_set = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100}
 my_list = sorted(my_set)
